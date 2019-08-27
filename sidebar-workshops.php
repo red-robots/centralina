aa70046cc0c354feb6bba92e6bcb0f239c26779b
Lisa <annalisa.quiamco@gmail.com>
2019-08-26 18:31:00 -0600

    Changed sidebar navigation on workshop single page

--- sidebar-workshops.php
+++ sidebar-workshops.php
@@ -8,15 +8,9 @@ $args = array(
 );
 $items = new WP_Query($args);
 if($items) { ?>
-<div id="left-sidebar">
+<div id="left-sidebar" class="sidebarNavs">
 	<div id="left-sidebar-subnav">
-		<ul>
-			<?php while ( $items->have_posts() ) : $items->the_post();  
-			$id = get_the_ID(); 
-			$is_active = ($id==$pageId) ? ' active':''; ?>
-			<li class="sb-item<?php echo $is_active?>"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></li>	
-			<?php endwhile; wp_reset_postdata(); ?>
-		</ul>
+		<?php wp_nav_menu( array( 'menu' => 15,'menu_id'=>'wellness' ) ); ?>
 	</div>
 </div>
 <?php } ?>
